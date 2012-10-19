33334f63ac9f3615022216ab046dc9e2eee3c683
Luca Fabbri <luca@keul.it>
2012-10-19 11:05:33 +0200

    Importazione iniziale progetto vuoto

---
+++ README.rst
@@ -0,0 +1,7 @@
+Introduzione
+============
+
+Questo prodotto contiene un esempio di configurazione di workflow e sicurezza per il libro
+`Plone: worfklow e sicurezza`__.
+
+__ http://plone-worfklow-e-sicurezza.readthedocs.org/
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='loremipsum.workflow',
+      version=version,
+      description='Prodotto di esempi di worfklow e sicurezza per libro '
+                  '"Plone: worfklow e sicurezza"',
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone workflow security example',
+      author='keul',
+      author_email='luca@keul.it',
+      url='https://github.com/keul/loremipsum.workflow',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['loremipsum'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
